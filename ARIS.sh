f5ae0e8f94c7268d28e4e15c36e7fe0d2180e640
MocDenny <denise.comincioli@studenti.unitn.it>
2026-06-12 15:16:24 +0200

    updated bash

--- ARIS.sh
+++ ARIS.sh
@@ -1,7 +1,8 @@
 # start python server
 source .venv/bin/activate
-python3 /BackendPy/vocalsystem.py
+python3 ./BackendPy/vocalsystem.py
 npm start
 # start chrome page in kiosk mode
-#exec DISPLAY=:0 XAUTHORITY=/root/.Xauthority chromium --no-sandbox --kiosk http://localhost:3000
+DISPLAY=:0 XAUTHORITY=/root/.Xauthority chromium --no-sandbox --kiosk http://localhost:3000
+
 
